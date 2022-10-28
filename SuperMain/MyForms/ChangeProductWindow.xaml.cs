ac87f2573fd371f93fba7cd71edc57e07192a8cf
awethy <lkmethy@DESKTOP-0R9KNSM>
2022-10-28 05:58:09 +0400

    впвпа

---
+++ SuperMain/MyForms/ChangeProductWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SuperMain.MyForms
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeProductWindow.xaml
+    /// </summary>
+    public partial class ChangeProductWindow : Window
+    {
+        public ChangeProductWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void btnOpenChange_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
